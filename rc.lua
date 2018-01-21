a263f4d6a910e34cc8ad90f61b81900704580cbf
Matt Lee <thatismatt@gmail.com>
2018-01-21 12:50:07 +0000

    Move floating layout to last.

--- rc.lua
+++ rc.lua
@@ -63,7 +63,6 @@ altkey = "Mod1"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-   awful.layout.suit.floating,
    awful.layout.suit.tile,
    awful.layout.suit.tile.left,
    awful.layout.suit.tile.bottom,
@@ -79,6 +78,7 @@ awful.layout.layouts = {
    -- awful.layout.suit.corner.ne,
    -- awful.layout.suit.corner.sw,
    -- awful.layout.suit.corner.se,
+   awful.layout.suit.floating,
 }
 -- }}}
 

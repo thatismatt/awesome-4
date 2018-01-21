50ecee40939e8002e119f4ab37a906fc2111295f
Matt Lee <thatismatt@gmail.com>
2018-01-21 10:56:33 +0000

    Setup mod keys and editor.

--- rc.lua
+++ rc.lua
@@ -55,17 +55,11 @@ prime.default_command_id = "d"
 -- {{{ Variable definitions
 beautiful.init("/home/matt/.config/awesome/theme.lua")
 
--- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
-editor = os.getenv("EDITOR") or "editor"
-editor_cmd = terminal .. " -e " .. editor
+emacs = "emacsclient -c -a="
 
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
+altkey = "Mod1"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {

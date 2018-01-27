dad609d388b9aa8717f0e74cc90c765a9ee6941a
Matt Lee <thatismatt@gmail.com>
2018-01-27 11:01:27 +0000

    Code tidying.

--- rc.lua
+++ rc.lua
@@ -414,7 +414,7 @@ for i = 1, 9 do
 end
 
 clientbuttons = gears.table.join(
-   awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
+   awful.button({        }, 1, function (c) client.focus = c ; c:raise() end),
    awful.button({ modkey }, 1, awful.mouse.client.move),
    awful.button({ modkey }, 3, awful.mouse.client.resize))
 

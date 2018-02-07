2bc1c2c129016097c53b051a954a3810b6e972f4
Matt Lee <thatismatt@gmail.com>
2018-02-07 16:02:53 +0000

    Kill client on task list middle click.

--- rc.lua
+++ rc.lua
@@ -187,6 +187,7 @@ local tasklist_buttons = gears.table.join(
             c:raise()
          end
    end),
+   awful.button({ }, 2, function (c) c:kill() end),
    awful.button({ }, 3, client_menu_toggle_fn()),
    awful.button({ }, 4, function () awful.client.focus.byidx(1) end),
    awful.button({ }, 5, function () awful.client.focus.byidx(-1) end))

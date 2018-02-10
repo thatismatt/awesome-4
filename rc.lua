5819034e07f24a0961b5f6955a4c556761d32c40
Matt Lee <thatismatt@gmail.com>
2018-02-10 19:36:04 +0000

    Rename mywibox -> wibox_top.

--- rc.lua
+++ rc.lua
@@ -293,10 +293,10 @@ awful.screen.connect_for_each_screen(function(s)
       s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons)
 
       -- Create the wibox
-      s.mywibox = awful.wibar({ position = "top", screen = s })
+      s.wibox_top = awful.wibar({ position = "top", screen = s })
 
       -- Add widgets to the wibox
-      s.mywibox:setup {
+      s.wibox_top:setup {
          layout = wibox.layout.align.horizontal,
          { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
